3bd29d15cc8f662d0f9e61e8c3b5f7459ad672cb
chetta chetta chetta <>
2019-04-10 22:13:10 +0700

    refactor point to variable

--- src/main/java/tennis/Tennis.java
+++ src/main/java/tennis/Tennis.java
@@ -2,23 +2,32 @@ package tennis;
 
 public class Tennis {
 
+    private static int zero = 0;
+
+    private static int fifteenPoint = 15;
+
+    private static int thirtyPoint = 30;
+
+    private static int fortyPoint = 40;
+
+
     public String getScore(int playerOneScore, int playerTwoScore) {
-        if(playerOneScore == 15 && playerTwoScore == 0) {
+        if(playerOneScore == fifteenPoint && playerTwoScore == zero) {
             return "fifteen,love";
         }
-        if(playerOneScore == 0 && playerTwoScore == 15) {
+        if(playerOneScore == zero && playerTwoScore == fifteenPoint) {
             return "love,fifteen";
         }
-        if(playerOneScore == 30 && playerTwoScore == 0) {
+        if(playerOneScore == thirtyPoint && playerTwoScore == zero) {
             return "thirty,love";
         }
-        if(playerOneScore == 0 && playerTwoScore == 30) {
+        if(playerOneScore == zero && playerTwoScore == thirtyPoint) {
             return "love,thirty";
         }
-        if(playerOneScore == 40 && playerTwoScore == 0) {
+        if(playerOneScore == fortyPoint && playerTwoScore == zero) {
             return "forty,love";
         }
-        if(playerOneScore == 0 && playerTwoScore == 40) {
+        if(playerOneScore == zero && playerTwoScore == fortyPoint) {
             return "love,forty";
         }
         return "all Love";
